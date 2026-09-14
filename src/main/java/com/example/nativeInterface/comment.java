f0ffb768b19613019f77be88a873b2970cd8ff69
seeker4 <resurgam4@outlook.com>
2026-09-15 01:00:21 +0800

    native interface usage

---
+++ src/main/java/com/example/nativeInterface/comment.java
@@ -0,0 +1,20 @@
+package com.example.nativeInterface;
+
+public class comment {
+    private String author;
+    private String text;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public void setAuthor(String author) {
+        this.author=author;
+    }
+    public void setText(String text) {
+        this.text=text;
+    }
+}
