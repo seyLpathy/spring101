c77e915b1b262a290a76deb2374d5de1a00655d7
seeker4 <resurgam4@outlook.com>
2026-09-19 11:31:11 +0800

    send data to restful endpoint

--- src/main/java/com/example/controller/PaymentController.java
+++ src/main/java/com/example/controller/PaymentController.java
@@ -4,22 +4,29 @@ import com.example.DTO.ErrorDetails;
 import com.example.DTO.PaymentDetails;
 import com.example.exceptions.NotEnoughMoneyException;
 import com.example.services.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
     private PaymentService paymentService;
+    private static Logger logger = LoggerFactory.getLogger(PaymentController.class);
+
+    @Autowired
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment(){
-            PaymentDetails paymentDetails = paymentService.processPayment();
-            return ResponseEntity
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails){
+        logger.info("received payment is {}", paymentDetails.getAmount());
+        return ResponseEntity
                     .status(HttpStatus.ACCEPTED)
                     .body(paymentDetails);
         }
