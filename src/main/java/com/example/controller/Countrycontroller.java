0f1bcf9750bba02324dfaecbab9864f84f8fa982
seeker4 <resurgam4@outlook.com>
2026-09-19 10:54:09 +0800

    responseentity usage

--- src/main/java/com/example/controller/Countrycontroller.java
+++ src/main/java/com/example/controller/Countrycontroller.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
 import com.example.DTO.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,8 +13,14 @@ import java.util.List;
 public class Countrycontroller {
 
     @GetMapping("/france")
-    public Country france(){
-        return Country.of("France",57);
+    public ResponseEntity<Country> france(){
+        Country c = Country.of("France",67);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent","Europe")
+                .header("capital","Paris")
+                .header("favorite food","cheese and wine")
+                .body(c);
 
     }
 
